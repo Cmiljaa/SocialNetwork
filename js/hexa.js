d37da0048456f740638177f1d92b4fc3dc853151
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-19 19:05:07 +0200

    Added profile and log out

--- js/hexa.js
+++ js/hexa.js
@@ -2,7 +2,21 @@ let session = new Session();
 session_id = session.getSession();
 
 if(session_id !== "") {
-    
+    async function populateUserData(){
+        let user = new User();
+        user = await user.get(session_id);
+
+        document.querySelector('#username').innerText = user.username;
+        document.querySelector('#email').innerText = user.email;
+    }
+
+    populateUserData()
+
 } else {
     window.location.href = "/";
 }
+
+document.querySelector('#logout').addEventListener('click', () =>{
+    session.destroySession();
+    window.location.href = 'index.html';
+})
