dfb01fe00bad44eadf04ab62a879e765ddfadf49
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-21 00:46:40 +0200

    Addedw how to change profile

--- js/hexa.js
+++ js/hexa.js
@@ -8,6 +8,9 @@ if(session_id !== "") {
 
         document.querySelector('#username').innerText = user.username;
         document.querySelector('#email').innerText = user.email;
+
+        document.querySelector('#korisnicko_ime').value = user.username;
+        document.querySelector('#edit_email').value = user.email;
     }
 
     populateUserData()
@@ -19,4 +22,34 @@ if(session_id !== "") {
 document.querySelector('#logout').addEventListener('click', () =>{
     session.destroySession();
     window.location.href = 'index.html';
+})
+
+document.querySelector('#editAccount').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'block';
+})
+
+document.querySelector('#closeModal').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'none';
+})
+
+document.querySelector('#editForm').addEventListener('submit', e => {
+    e.preventDefault();
+
+    let user = new User();
+    user.username = document.querySelector('#korisnicko_ime').value;
+    user.email = document.querySelector('#edit_email').value;
+    user.edit();
+});
+
+document.querySelector('#deleteProfile').addEventListener('click', e =>{
+    e.preventDefault();
+
+    let text = 'Da li ste sigurni da zaista zelite da obrisete profil?';
+
+    if(confirm(text) === true)
+    {
+        let user = new User();
+
+        user.delete();
+    }
 })
