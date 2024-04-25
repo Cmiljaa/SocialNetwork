53fec3a94c357a6db0ab9f4621b1a23a25a3e957
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-25 19:31:41 +0200

    Added create Post

--- js/hexa.js
+++ js/hexa.js
@@ -2,29 +2,70 @@ let session = new Session();
 session_id = session.getSession();
 
 if(session_id !== "") {
-
-    async function populateUserData() {
+    async function populateUserData(){
         let user = new User();
-        user = await user.get(session_id); 
+        user = await user.get(session_id);
 
-        document.querySelector('#username').innerText = user['username'];
-        document.querySelector('#email').innerText = user['email'];
-
-        document.querySelector('#korisnicko_ime').value = user['username'];
-        document.querySelector('#edit_email').value = user['email'];
         document.querySelector('#username').innerText = user.username;
         document.querySelector('#email').innerText = user.email;
+
+        document.querySelector('#korisnicko_ime').value = user.username;
+        document.querySelector('#edit_email').value = user.email;
     }
 
-    populateUserData();
+    populateUserData()
 
 } else {
     window.location.href = "/";
 }
 
-document.querySelector('#logout').addEventListener('click', e => {
+document.querySelector('#logout').addEventListener('click', () =>{
+    session.destroySession();
+    window.location.href = 'index.html';
+})
+
+document.querySelector('#editAccount').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'block';
+})
+
+document.querySelector('#closeModal').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'none';
+})
+
+document.querySelector('#editForm').addEventListener('submit', e => {
     e.preventDefault();
 
-    session.destroySession();
-    window.location.href = '/';
+    let user = new User();
+    user.username = document.querySelector('#korisnicko_ime').value;
+    user.email = document.querySelector('#edit_email').value;
+    user.edit();
+});
+
+document.querySelector('#deleteProfile').addEventListener('click', e =>{
+    e.preventDefault();
+
+    let text = 'Da li ste sigurni da zaista zelite da obrisete profil?';
+
+    if(confirm(text) === true)
+    {
+        let user = new User();
+
+        user.delete();
+    }
 })
+
+document.querySelector('#postForm').addEventListener('submit', e =>{
+    e.preventDefault();
+
+    async function createPost(){
+        let content = document.querySelector('#postContent').value;
+        let post = new Post();
+        post.post_content = content;
+        post = await post.create();
+
+        console.log(post);
+    }
+
+    createPost();
+})
+
